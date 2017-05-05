5e332dba8e0ff76e8510adbb951333498d184106
Denn Mark Laurente <dennmarklaurente@gmail.com>
2017-05-05 17:22:07 -0400

    Not really for AI (for Player controls)

---
+++ Assets/AIScripts/AiController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiController : MonoBehaviour {
+
+	public bool isSelected;
+    // public bool canMove;
+    
+    void Start(){
+        isSelected = false;
+        transform.GetChild (0).gameObject.SetActive(true);
+        transform.GetChild (1).gameObject.SetActive(true);
+        transform.GetChild (2).gameObject.SetActive(true);
+        transform.GetChild (3).gameObject.SetActive(true);
+    }
+    
+    void Update() {
+       tileVisible();
+    }
+
+    void OnMouseDown() {
+        isSelected = !isSelected;
+    }
+
+    void tileVisible() {
+         if(isSelected){
+            transform.GetChild (0).gameObject.SetActive(true);
+            transform.GetChild (1).gameObject.SetActive(true);
+            transform.GetChild (2).gameObject.SetActive(true);
+            transform.GetChild (3).gameObject.SetActive(true);
+        } else{
+            transform.GetChild (0).gameObject.SetActive(false);
+            transform.GetChild (1).gameObject.SetActive(false);
+            transform.GetChild (2).gameObject.SetActive(false);
+            transform.GetChild (3).gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/AIScripts/TileMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMove : MonoBehaviour {
+
+	public Transform target;
+	public GameObject player;
+	// public ScriptType aiScript;
+
+	// void Start() {
+	// 	 aiScript = GameObject.Find("AiController").GetComponent<ScriptType>();
+	// }
+
+	void OnMouseDown()
+	{
+		player.transform.position = Vector3.MoveTowards(transform.position, target.position, 1f * Time.deltaTime);
+
+		// aiScript.isSelected = true;
+	}
+}
