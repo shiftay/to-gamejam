50d45cb095af149f4cb2a86fca48e0c0fce1a3f2
shiftay <shroebuck@gmail.com>
2017-05-05 16:22:44 -0400

    Fixed Options loading improperly

--- Assets/xml_test/Options.cs
+++ Assets/xml_test/Options.cs
@@ -3,11 +3,11 @@ using System.Xml.Serialization;
 
 public class Options {
 	[XmlElementAttribute("Animations")]
-	private bool isAnimated;
+	public bool isAnimated;
 	[XmlElementAttribute("SFXVol")]
-	private int sfx_volume;
+	public int sfx_volume;
 	[XmlElementAttribute("MUSICVol")]
-	private int music_volume;
+	public int music_volume;
 
 	public bool GetAnim() { return isAnimated; }
 	public int GetSFX() { return sfx_volume; }
