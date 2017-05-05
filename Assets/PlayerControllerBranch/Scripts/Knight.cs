283efca342c71854f4c68e00b8d89a87e7cae760
Giane Carlo Go <gcarlo_1990@yahoo.com>
2017-05-05 14:59:51 -0400

    It's Something

---
+++ Assets/PlayerControllerBranch/Scripts/Knight.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour {
+
+	public int hitPoints;
+	public int magicPoints;
+	public int speed;
+	public bool currentUnit;
+	public GameObject[]otherUnits;
+	
+	void Awake(){
+		hitPoints = 100;
+		magicPoints = 30;
+		speed = 1;
+		currentUnit = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(currentUnit){		
+			isSelected();
+		} else {
+			notSelected();
+		}
+	}
+	void OnMouseDown(){
+		for(int i = 0; i < 1; i++){
+			otherUnits[0].GetComponent<Archer>().currentUnit = false;
+		}
+		currentUnit = true;
+	}
+
+	void isSelected(){
+			transform.GetChild (0).gameObject.SetActive(true);
+			transform.GetChild (1).gameObject.SetActive(true);
+			transform.GetChild (2).gameObject.SetActive(true);
+			transform.GetChild (3).gameObject.SetActive(true);
+	}
+	void notSelected(){
+			transform.GetChild (0).gameObject.SetActive(false);
+			transform.GetChild (1).gameObject.SetActive(false);
+			transform.GetChild (2).gameObject.SetActive(false);
+			transform.GetChild (3).gameObject.SetActive(false);
+	}
+}
