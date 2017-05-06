a7dc2d1cf75eba7dd7a092f06f4857e0bc9e53ca
Denn Mark Laurente <dennmarklaurente@gmail.com>
2017-05-06 15:04:41 -0400

    Testing grid generation with buttons

---
+++ Assets/GridScripts/GridScript.cs
@@ -0,0 +1,25 @@
+﻿ using UnityEngine;
+ using UnityEngine.UI;
+ using System.Collections;
+
+ public class GridScript : MonoBehaviour {
+     public int rows = 4;
+     public int columns = 4;
+     private float buttonWidth;                                        //Change
+     private float buttonHeight;                                        //Change
+     public Button prefab;
+     private Button button;
+     void Start() {
+         RectTransform myRect = GetComponent<RectTransform>();        //Change
+         buttonHeight = myRect.rect.height / (float)rows;            //Change
+         buttonWidth = myRect.rect.width / (float)columns;            //Change
+         GridLayoutGroup grid = this.GetComponent<GridLayoutGroup>();
+         grid.cellSize = new Vector2(buttonWidth, buttonHeight);
+         for (int i = 0; i < rows; i++) {
+             for (int j = 0; j < columns; j++) {
+                 button = (Button)Instantiate(prefab);
+                 button.transform.SetParent(transform, false);        //Change
+             }
+         }
+     }
+ }
